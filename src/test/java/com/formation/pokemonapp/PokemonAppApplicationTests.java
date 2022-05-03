4ba9492a4de83a88a83578ebef1eb2534b9a0c61
anh-k <68654676+anh-k@users.noreply.github.com>
2022-05-03 16:26:57 +0200

    add test params (#7)

--- src/test/java/com/formation/pokemonapp/PokemonAppApplicationTests.java
+++ src/test/java/com/formation/pokemonapp/PokemonAppApplicationTests.java
@@ -1,9 +1,16 @@
 package com.formation.pokemonapp;
 
+import io.cucumber.spring.CucumberContextConfiguration;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@ActiveProfiles("test")
+@CucumberContextConfiguration
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 class PokemonAppApplicationTests {
 
 	@Test
